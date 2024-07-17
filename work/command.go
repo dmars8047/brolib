d9435268dd9e492244aab8192ce7f6bff5c48844
dmars8047 <dmars8047@live.com>
2024-07-16 22:32:49 -0400

    room deletion command now has room object

--- work/command.go
+++ work/command.go
@@ -60,8 +60,8 @@ type ProcessFriendRequestAcceptanceCommand struct {
 
 // Contains all necessary data to process a command of type COMMAND_TYPE_PROCESS_ROOM_DELETION.
 type ProcessRoomDeletionCommand struct {
-	// The ID of the room that was deleted.
-	RoomId string `json:"room_id"`
+	// The room to be deleted.
+	Room chat.Room `json:"room"`
 }
 
 // Contains all necessary data to process a command of type COMMAND_TYPE_PROCESS_USER_ONLINE_STATUS_CHANGE.
