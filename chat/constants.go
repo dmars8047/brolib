508ebf939bd0685b3eda260d52d87c2f1f86e78f
dmars8047 <dmars8047@live.com>
2024-07-16 20:01:49 -0400

    dropping support for friends_only membership model for rooms - will likely bring it back later

--- chat/constants.go
+++ chat/constants.go
@@ -44,7 +44,7 @@ type RoomMembershipModel string
 
 const (
 	// The owner's friends will be allowed to join the room.
-	FRIENDS_MEMBERSHIP_MODEL RoomMembershipModel = "friends"
+	// FRIENDS_MEMBERSHIP_MODEL RoomMembershipModel = "friends_only"
 	// The room is public. Anyone can join.
 	PUBLIC_MEMBERSHIP_MODEL RoomMembershipModel = "public"
 )
